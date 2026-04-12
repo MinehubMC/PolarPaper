72f2eba172839af02195808626c05488337ba7fd
emortal <emortaldev@proton.me>
2026-04-12 20:17:25 +0100

    complete future after entity chunk has been initialized

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     alias(libs.plugins.hangar.publish)
 }
 
-val developmentVersion = "${libs.versions.minecraft.get()}.23"
+val developmentVersion = "${libs.versions.minecraft.get()}.24"
 
 version = getVersion()
 group = "live.minehub"
