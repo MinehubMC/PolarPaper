c2991db2c713765e2f4ff49508dbd711a919b9ed
emortal <emortaldev@proton.me>
2026-04-17 17:20:01 +0100

    refresh schematic chunks after adding block entities sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     alias(libs.plugins.hangar.publish)
 }
 
-val developmentVersion = "${libs.versions.minecraft.get()}.28"
+val developmentVersion = "${libs.versions.minecraft.get()}.29"
 
 version = getVersion()
 group = "live.minehub"
